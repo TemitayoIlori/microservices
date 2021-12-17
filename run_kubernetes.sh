11e4d61c345e10badd06b6fe3ab4e5e97291f4c4
Temitayo Ilori <57290886+TemitayoIlori@users.noreply.github.com>
2021-12-17 01:06:55 -0500

    Update run_kubernetes.sh

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -2,18 +2,18 @@
 # This is your Docker ID/path
 #alias kubectl="minikube kubectl --"
 
-dockerpath=temmysam/udacity:latest
+minikube dockerpath=temmysam/udacity
 
 # Step 2
 # Run the Docker Hub container with kubernetes
 #kubectl run NAME --image=DOCKER_IMAGE --port=YOUR_PORT
-kubectl run udacity --image=$dockerpath
+minikube kubectl run udacity image=$dockerpath
 
 
 # Step 3:
 # List kubernetes pods
-kubectl get pods
+minikube kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-kubectl port-forward pod/udacity 8000:80
+minikube kubectl port-forward pod/udacity 8000:80
